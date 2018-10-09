621ecdcc4b75549c54ac42256923d459589b1ad4
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-10-09 20:34:28 +0800

    modify: style login page with bootstrap

--- views/user/Login.jsx
+++ views/user/Login.jsx
@@ -4,18 +4,24 @@ class HomePage extends React.Component {
   render() {
     return (
         <DefaultLayout title="Home">
+            <div className="container">
             <form method="POST" action="/login">
-                <h1>Login</h1>
-                <div className="login">
-                    Name: <input name="name" type="text" />
+                <h1 className="form-heading">Login</h1>
+                <p>Please enter your username and password</p>
+                <div className="login form-group">
+                    Name: <input name="name" className="form-control" type="text" />
                 </div>
                 <br/>
-                <div className="login">
-                    Password: <input name="password" type="password" />
+                <div className="login form-group">
+                    Password: <input name="password" className="form-control" type="password" />
+                </div>
+                <div className="forget">
+                    <a href="/#">Forget Password?</a>
                 </div>
                 <br/>
-                <input name="submit" type="submit" />
+                <input name="submit" className="btn btn-primary"type="submit" />
             </form>
+            </div>
         </DefaultLayout>
     );
   }
