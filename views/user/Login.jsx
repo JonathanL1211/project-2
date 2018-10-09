097e99297acb92cff78e4be17eadaf4ea94f829e
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-10-09 20:36:54 +0800

    modify: bootstrap offset login page to the center

--- views/user/Login.jsx
+++ views/user/Login.jsx
@@ -5,22 +5,26 @@ class HomePage extends React.Component {
     return (
         <DefaultLayout title="Home">
             <div className="container">
-            <form method="POST" action="/login">
-                <h1 className="form-heading">Login</h1>
-                <p>Please enter your username and password</p>
-                <div className="login form-group">
-                    Name: <input name="name" className="form-control" type="text" />
+            <div className="row">
+                <div className="col-md-4 offset-md-4">
+                    <form method="POST" action="/login">
+                        <h1 className="form-heading">Login</h1>
+                        <p>Please enter your username and password</p>
+                        <div className="login form-group">
+                            Name: <input name="name" className="form-control" type="text" />
+                        </div>
+                        <br/>
+                        <div className="login form-group">
+                            Password: <input name="password" className="form-control" type="password" />
+                        </div>
+                        <div className="forget">
+                            <a href="/#">Forget Password?</a>
+                        </div>
+                        <br/>
+                        <input name="submit" className="btn btn-primary"type="submit" />
+                    </form>
                 </div>
-                <br/>
-                <div className="login form-group">
-                    Password: <input name="password" className="form-control" type="password" />
-                </div>
-                <div className="forget">
-                    <a href="/#">Forget Password?</a>
-                </div>
-                <br/>
-                <input name="submit" className="btn btn-primary"type="submit" />
-            </form>
+            </div>
             </div>
         </DefaultLayout>
     );
