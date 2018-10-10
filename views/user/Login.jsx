e8363974b603e93ff958edda566b287d6ba1ca2f
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-10-10 10:01:13 +0800

    modify: add borders on the login page

--- views/user/Login.jsx
+++ views/user/Login.jsx
@@ -5,24 +5,28 @@ class HomePage extends React.Component {
     return (
         <DefaultLayout title="Home">
             <div className="container">
-            <div className="row">
+            <div className="row mt-4">
                 <div className="col-md-4 offset-md-4">
-                    <form method="POST" action="/login">
-                        <h1 className="form-heading" style={ {marginTop: '20px'} }>Login</h1>
-                        <p>Please enter your username and password</p>
-                        <div className="login form-group">
-                            Name: <input name="name" className="form-control" type="text" />
+                    <div className="card border-dark border rounded"} >
+                        <div className="card-body">
+                            <form method="POST" action="/login">
+                                <h1 className="form-heading" style={ {marginTop: '20px'} }>Login</h1>
+                                <p>Please enter your username and password</p>
+                                <div className="login form-group">
+                                    Name: <input name="name" className="form-control" type="text" />
+                                </div>
+                                <br/>
+                                <div className="login form-group">
+                                    Password: <input name="password" className="form-control" type="password" />
+                                </div>
+                                <div className="forget">
+                                    <a href="/#">Forget Password?</a>
+                                </div>
+                                <br/>
+                                <input name="submit" className="btn btn-primary"type="submit" />
+                            </form>
                         </div>
-                        <br/>
-                        <div className="login form-group">
-                            Password: <input name="password" className="form-control" type="password" />
-                        </div>
-                        <div className="forget">
-                            <a href="/#">Forget Password?</a>
-                        </div>
-                        <br/>
-                        <input name="submit" className="btn btn-primary"type="submit" />
-                    </form>
+                    </div>
                 </div>
             </div>
             </div>
