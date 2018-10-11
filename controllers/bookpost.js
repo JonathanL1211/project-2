d36ac2373ad9da8eb05459225ecc98767a0c5936
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-10-11 15:06:34 +0800

    add: postId cookie to track the post number

--- controllers/bookpost.js
+++ controllers/bookpost.js
@@ -14,7 +14,7 @@ module.exports = (db) => {
    const createPost = (request, response) => {
       db.bookpost.createPost(request.body, request.cookies, (error, queryResult) => {
         console.log("request.body for CREATEPOST-------------------------------: ", request.body);
-        //sconsole.log('QUERYRESULTS NEW POST -----------------**********------: ', queryResult.rows);
+        console.log('QUERYRESULTS NEW POST -----------------**********------: ', queryResult.rows);
         if (error) {
             console.error('error getting user:', error);
             response.sendStatus(500);
@@ -28,16 +28,21 @@ module.exports = (db) => {
         } else {
             console.log('Post not created!');
         }
-
+        response.cookie('postId', queryResult.rows[0].id);
         // redirect to home page after creation
         response.redirect('/profile/' + request.cookies['userId']);
 
       })
   }
 
+  const displayPostPage = (request, response) =>{
+
+  };
+
   return{
       newPostForm,
-      createPost
+      createPost,
+      displayPostPage
   };
 
 };
